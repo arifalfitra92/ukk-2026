c8e6e9c559e8553e78f4e9a1f3463d95fd34e6a1
arifalfitra92 <arifalfitra2809@gmail.com>
2026-09-03 02:16:17 -0700

    table baru

---
+++ resources/views/kategori/index.sakuci.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <h1>daftar kategori</h1>
+    <table class="table table-bordered table-striped">
+        <thead>
+            <tr>
+                <th>NO</th>
+                <th>Kode Kategori</th>
+                <th>Nama Kategori</th>
+                <th>Keterangan</th>
+                <th>aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+         @foreach ($data as $kategori)
+                <tr>   
+                 <td>{{ $kategori->id_kategori}}</td>
+                 <td>{{ $kategori->kode_kategori}}</td>
+                 <td>{{ $kategori->nama_kategori}}</td>
+                 <td>{{ $kategori->keterangan}}</td>
+                 <td>
+                    <a href="" class="btn btn-primary btn-sm">Edit</a>
+                    <a href="" class="btn btn-danger btn_sm">Hapus</a>
+                 </td> 
+                </tr>
+             @endforeach
+            </tbody>
+        </table>
+
+        {!! $data->links() !!}
+    </div>
+@endsection                    
