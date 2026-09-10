cf031f738713c23c79b681533ca0b4100331d3f5
zazangweh <jajangalbi04@gmail.com>
2026-09-10 08:22:32 +0700

    tambah data

--- app/Controllers/KategoriController.php
+++ app/Controllers/KategoriController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controllers;
+
 use Sakuci\Controller;
 use Sakuci\Http\Request;
 use App\Models\Kategori;
@@ -9,23 +10,21 @@ class KategoriController extends Controller
 {
     public function index(Request $request)
     {
-        $data = Kategori::orderBy('id_kategori', 'desc')->paginate(10);
-        return view('kategori.index', compact('data'));
+        $data = Kategori::orderBy('id_kategori', 'desc')->paginate(2);
+       return view('kategori.index', compact('data'));
     }
-    public function create (request $request)
+    public function create(Request $request)
     {
-       return view('kategori.create');
+        return view('kategori.create');
     }
     public function store(Request $request)
     {
-        $request->validate([
-            'nama' => 'required|string|max:255'
+        $data = $request->validate([
+            'nama_kategori' => 'required|min:3|max:100',
+            'kode_kategori' => 'required|varchar|min:3|max:100',
+            'keterangan' => 'required|min:3|max:100',
         ]);
-
-        kategori::create([
-            'keterangan' => $request->input('nama')
-        ]);
-
-        return redirect()->route('kategori.index')->with('succsess', 'kategori berhasil ditambahkan');
-    } 
+        Kategori::create($data);
+        return redirect(route('kategori.index'))->with('success', 'Kategori berhasil ditambahkan.');
+    }
 }
--- resources/views/kategori/index.sakuci.php
+++ resources/views/kategori/index.sakuci.php
@@ -1,37 +1,32 @@
 @extends('layouts.app')
 
-@section('content')
-<h1>kategori</h1>
-<a href="{{ route('kategori.create') }}" class="btn btn-primary mb-3">Tambah Kategori</a>
-<table class="table table-striped table-hover">
-<div class="container">
-    <h1>daftar kategori</h1>
-    <table class="table table-bordered table-striped">
-        <thead>
-            <tr>
-                <th>NO</th>
-                <th>Kode Kategori</th>
-                <th>Nama Kategori</th>
-                <th>Keterangan</th>
-                <th>aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-         @foreach ($data as $kategori)
-                <tr>   
-                 <td>{{ $kategori->id_kategori}}</td>
-                 <td>{{ $kategori->kode_kategori}}</td>
-                 <td>{{ $kategori->nama_kategori}}</td>
-                 <td>{{ $kategori->keterangan}}</td>
-                 <td>
-                    <a href="" class="btn btn-primary btn-sm">Edit</a>
-                    <a href="" class="btn btn-danger btn_sm">Hapus</a>
-                 </td> 
-                </tr>
-             @endforeach
-            </tbody>
-        </table>
+@section('title', config('app.name') . ' -- Kerangka PHP Ringan')
 
-        {!! $data->links() !!}
-    </div>
-@endsection                    
+@section('content')
+<h1>Daftar Kategori</h1>
+<a href="{{ route('kategori.create') }}" class="btn btn-danger mb-3">Tambah Kategori</a>
+
+<table class = "table table-sm align-middle"> 
+<tr>
+    <th>No</th>
+    <th>nama kategori</th>
+    <th>kode kategori</th>
+    <th>Keterangan</th>
+    <th>Aksi</th>
+</tr>
+@php $no = 1; @endphp
+@foreach ($data as $kategoris)
+<tr>
+    <td> {{ $no++ }} </td>
+    <td> {{ $kategoris->nama_kategori }} </td>
+    <td> {{ $kategoris->kode_kategori }} </td>
+    <td> {{ $kategoris->keterangan }} </td>
+    <td>
+         <a href="" class="btn btn-primary btn-sm">Edit</a>
+         <a href="" class="btn btn-danger btn-sm">Hapus</a>
+    </td>
+</tr>
+@endforeach
+</table>
+{!! $data->links() !!}
+@endsection
